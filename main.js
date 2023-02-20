d202a19e3421d83d6cdbccf4840b8b435bb25060
Skeleton-2010 <95185689+Skeleton-2010@users.noreply.github.com>
2023-02-20 15:07:26 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+let Input;
+
+console.log("1-st")
+
+for (let Count = 3; Count <= 20; Count++) {
+  console.log(Count)
+}
+
+console.log("2-nd");
+
+for (let Count = 10; Count <= 20; Count++) {
+  console.log(Count);
+}
+
+console.log("3-rd");
+
+let A = prompt("Number A?");
+let B = prompt("Number B?");
+
+for (let Count = A; Count <= B; Count++) {
+  console.log(Count);
+}
